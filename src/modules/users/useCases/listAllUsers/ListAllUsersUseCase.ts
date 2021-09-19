1e5a329c7b4d22247196150824372830235be27d
TaizAlves <taizralves@gmail.com>
2021-09-19 14:52:16 -0300

    all steps done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,11 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user) throw new Error("User do not exists");
+    if (!user.admin) throw new Error("User is not an administrator");
+
+    return this.usersRepository.list();
   }
 }
 
